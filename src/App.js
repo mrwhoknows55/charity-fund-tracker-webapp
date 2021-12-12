7cfe3fc79f274d4d7c799972ddd73c8bffdd8d20
jaypowar00 <jaypowar00@gmail.com>
2021-12-12 13:47:07 +0530

    Added Donor Register & Login

--- src/App.js
+++ src/App.js
@@ -1,27 +1,39 @@
-import React from 'react';
+import React, { PureComponent } from 'react'
+import { BrowserRouter as Switch, Route } from 'react-router-dom';
 import {
-  ChakraProvider,
-  Box,
-  VStack,
-  Grid,
-  theme,
-  Heading,
+    ChakraProvider,
+    Box,
+    // VStack,
+    Grid,
+    theme,
+    // Heading,
 } from '@chakra-ui/react';
-import { ColorModeSwitcher } from './ColorModeSwitcher';
+// import { ColorModeSwitcher } from './ColorModeSwitcher';
+import Home from './Components/Home';
+import Navbar from './Components/Nav/navbar';
+import Login from './Components/Login';
+import Register from './Components/Register';
 
-function App() {
-  return (
-    <ChakraProvider theme={theme}>
-      <Box textAlign='center' fontSize='xl'>
-        <Grid minH='100vh' p={3}>
-          <ColorModeSwitcher justifySelf='flex-end' />
-          <VStack spacing={8}>
-            <Heading as={'h1'}>Charity Fund Tracking Web App</Heading>
-          </VStack>
-        </Grid>
-      </Box>
-    </ChakraProvider>
-  );
+class App extends PureComponent {
+    render() {
+        return (
+            <>
+                <ChakraProvider theme={theme}>
+                    <Box textAlign='center' fontSize='xl'>
+                        <Navbar>
+                            <Grid minH='100vh' p={3}>
+                                <Switch>
+                                    <Route exact path='/' component={Home}/>
+                                    <Route exact path='/login' component={Login}/>
+                                    <Route exact path='/register' component={Register}/>
+                                </Switch>
+                            </Grid>
+                        </Navbar>
+                    </Box>
+                </ChakraProvider>
+            </>
+        )
+    }
 }
 
 export default App;
