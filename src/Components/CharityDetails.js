6247e8aab91c6af0d69642ac3e7b7df208550d2d
Avadhut Tanugade <avdhuttanugade55@gmail.com>
2022-01-12 21:18:03 +0530

    charitydetails added

--- src/Components/CharityDetails.js
+++ src/Components/CharityDetails.js
@@ -1,4 +1,4 @@
-import { Center, Heading, SimpleGrid, VStack } from '@chakra-ui/react';
+import { Box, Center, Flex, Heading, HStack, Image, SimpleGrid, Square, Text, VStack } from '@chakra-ui/react';
 import { useParams } from 'react-router-dom';
 import React, { useEffect, useState } from 'react';
 import axios from 'axios';
@@ -22,15 +22,30 @@ function CharityDetails() {
   }, []);
 
   return (<>
-    {/*<Navbar />*/}
     <VStack spacing={8}>
-      <Center py={12} marginTop={90}>
-        <SimpleGrid columns={4} spacing={10}>
-          <Heading>
-            {charity.name}
-          </Heading>
-        </SimpleGrid>
-      </Center>
+      <VStack py={'2vh'} marginTop={'2vh'}>
+        <Flex px={'2vw'} w={'100vw'} h={'35vh'} color='white'>
+          <Flex>
+            <HStack spacing={'5vw'}>
+              <Image marginStart={'2vw'} w='12vw' src={charity.profile_image} borderRadius='full' />
+              <VStack alignItems={'flex-start'}>
+                <Heading mb={'2vh'}>
+                  {charity.name}
+                </Heading>
+                <Text>
+                  Founded on: {charity.founded_in}
+                </Text>
+                <Text>
+                  Total Funding: {charity.total_fundings}
+                </Text>
+                <Text>
+                  Total expenditure: {charity.total_expenditure}
+                </Text>
+              </VStack>
+            </HStack>
+          </Flex>
+        </Flex>
+      </VStack>
     </VStack>
   </>);
 }
