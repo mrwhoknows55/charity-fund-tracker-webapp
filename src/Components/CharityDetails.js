c5c66fd464a089c213f676c567e7b92f6738fbb1
Avadhut Tanugade <avdhuttanugade55@gmail.com>
2022-06-11 20:31:55 +0530

    Added Shimmer Loading Effect In Charity Details Page

--- src/Components/CharityDetails.js
+++ src/Components/CharityDetails.js
@@ -1,8 +1,5 @@
 import {
   Button,
-  Editable,
-  EditableInput,
-  EditablePreview,
   Flex,
   Heading,
   HStack,
@@ -12,7 +9,9 @@ import {
   VStack,
   InputGroup,
   Input,
-  InputLeftAddon,
+  Skeleton,
+  SkeletonText,
+  SkeletonCircle,
 } from '@chakra-ui/react';
 import { useParams } from 'react-router-dom';
 import React, { useEffect, useState } from 'react';
@@ -50,6 +49,7 @@ function CharityDetails() {
   const [account, setAccount] = useState('');
   const [smartContractLoaded, setSmartContractLoaded] = useState(false);
   const [ethAmount, setEthAmount] = useState('');
+  const [isLoading, setLoading] = useState(true);
   const access_token = window.sessionStorage.getItem('access_token');
 
   const [selectedCurrency, setSelectedCurrency] = useState({
@@ -91,11 +91,15 @@ function CharityDetails() {
         if (response.data.status) {
           console.log(response.data);
           setCharity(response.data.charity);
+          setTimeout(() => {
+            setLoading(false);
+          }, 2000);
         }
       })
       .catch(err => {
         alert('Something went wrong');
         console.log('Error: ' + err.message);
+        setLoading(false);
       });
   }, []);
 
@@ -129,21 +133,24 @@ function CharityDetails() {
   }
 
   async function loadSmartConrtact(e) {
-      let web3 = window.web3
-      const networkId = await web3.eth.net.getId()
-      const fundEthData = fundEth.networks[networkId]
-      if(fundEthData) {
-          const fundEth_Contract = await new web3.eth.Contract(fundEth.abi, fundEthData.address)
-          setContractAddress(fundEthData.address);
-          setFundEthContract(fundEth_Contract);
-          setSmartContractLoaded(true);
-      } else {
-          window.alert('fundEth contract not deployed to detected network.')
-      }
+    let web3 = window.web3;
+    const networkId = await web3.eth.net.getId();
+    const fundEthData = fundEth.networks[networkId];
+    if (fundEthData) {
+      const fundEth_Contract = await new web3.eth.Contract(
+        fundEth.abi,
+        fundEthData.address
+      );
+      setContractAddress(fundEthData.address);
+      setFundEthContract(fundEth_Contract);
+      setSmartContractLoaded(true);
+    } else {
+      window.alert('fundEth contract not deployed to detected network.');
+    }
   }
 
   async function sendEth(ethAmount) {
-    console.log("eth : " + ethAmount)
+    console.log('eth : ' + ethAmount);
     if (smartContractLoaded) {
       try {
         await fundEthContract.methods
@@ -155,30 +162,33 @@ function CharityDetails() {
             user.user_id,
             charity.name,
             charity.user_id
-          ).send(
-            {
-              from: account,
-              value: Web3.utils.toWei(ethAmount, 'ether'),
-            }
-          ).then(async res => {
-              console.log(res)
-              console.log("transaction successfull.")
-              window.web3.eth.getBalance((account)).then(value => {
-                //  console.log("account balance: " + window.web3.utils.fromWei( value , 'ether') + " ETH"); 
-                //  alert("transaction successfull (balance: "+window.web3.utils.fromWei( value , 'ether')+")")
-                });
-          }).catch(async err => {
-              console.log(err)
-              console.log(err.message);
-        }) 
+          )
+          .send({
+            from: account,
+            value: Web3.utils.toWei(ethAmount, 'ether'),
+          })
+          .then(async res => {
+            console.log(res);
+            console.log('transaction successfull.');
+            window.web3.eth.getBalance(account).then(value => {
+              //  console.log("account balance: " + window.web3.utils.fromWei( value , 'ether') + " ETH");
+              //  alert("transaction successfull (balance: "+window.web3.utils.fromWei( value , 'ether')+")")
+            });
+          })
+          .catch(async err => {
+            console.log(err);
+            console.log(err.message);
+          });
       } catch (error) {
         if (error.name === 'TypeError') {
-          alert('Please login to use Donate feature!!!')
-          document.location.href = '/login'
+          alert('Please login to use Donate feature!!!');
+          document.location.href = '/login';
         }
       }
-    }else{
-      alert("SmartContract not loaded successfully, please contact the developer of this website if feel necessary...")
+    } else {
+      alert(
+        'SmartContract not loaded successfully, please contact the developer of this website if feel necessary...'
+      );
     }
   }
 
@@ -206,7 +216,15 @@ function CharityDetails() {
           const mult = response.data.ETH;
           const convertedEthValue = eval(ethAmount * mult);
           console.log('convertedEthValue: ' + convertedEthValue);
-          sendEth(convertedEthValue.toString().substr(0, convertedEthValue.toString().length-(convertedEthValue.toString().length-20)));
+          sendEth(
+            convertedEthValue
+              .toString()
+              .substr(
+                0,
+                convertedEthValue.toString().length -
+                  (convertedEthValue.toString().length - 20)
+              )
+          );
         } else {
           sendEth(ethAmount.toString());
         }
@@ -237,88 +255,102 @@ function CharityDetails() {
               justifyContent={'space-evenly'}
             >
               <HStack spacing={'2vw'}>
-                <Image
-                  marginStart={'2vw'}
-                  w="12vw"
-                  src={charity.profile_image}
-                  borderRadius="full"
-                />
-                <VStack alignItems={'flex-start'}>
-                  <Heading mb={'2vh'} fontSize={'46'}>
-                    {charity.name}
-                  </Heading>
-                  <Text>
-                    <b>Founded on:</b>{' '}
-                    {charity.charityDetails && charity.charityDetails.founded_in
-                      ? charity.charityDetails.founded_in
-                      : ''}
-                  </Text>
-                  <Text>
-                    Total Funding: ₹{' '}
-                    {charity.charityDetails &&
-                    charity.charityDetails.total_fundings
-                      ? charity.charityDetails.total_fundings
-                      : ``}
-                  </Text>
-                  <Text>
-                    Total expenditure: ₹{' '}
-                    {charity.charityDetails &&
-                    charity.charityDetails.total_expenditure
-                      ? charity.charityDetails.total_expenditure
-                      : ``}
-                  </Text>
-                </VStack>
+                <SkeletonCircle
+                  height={'12vw'}
+                  width={'12vw'}
+                  isLoaded={!isLoading}
+                >
+                  <Image
+                    w="12vw"
+                    src={charity.profile_image}
+                    borderRadius="full"
+                  />
+                </SkeletonCircle>
+                <SkeletonText
+                  isLoaded={!isLoading}
+                  mb={'2vh'}
+                  noOfLines={6}
+                  spacing="4"
+                >
+                  <VStack alignItems={'flex-start'}>
+                    <Heading mb={'2vh'} fontSize={'46'}>
+                      {charity.name}
+                    </Heading>
+                    <Text>
+                      <b>Founded on:</b>{' '}
+                      {charity.charityDetails &&
+                      charity.charityDetails.founded_in
+                        ? charity.charityDetails.founded_in
+                        : ''}
+                    </Text>
+                    <Text>
+                      Total Funding: ₹{' '}
+                      {charity.charityDetails &&
+                      charity.charityDetails.total_fundings
+                        ? charity.charityDetails.total_fundings
+                        : ``}
+                    </Text>
+                    <Text>
+                      Total expenditure: ₹{' '}
+                      {charity.charityDetails &&
+                      charity.charityDetails.total_expenditure
+                        ? charity.charityDetails.total_expenditure
+                        : ``}
+                    </Text>
+                  </VStack>
+                </SkeletonText>
               </HStack>
               <VStack>
-                <Select
-                  defaultOptions
-                  value={selectedCurrency}
-                  options={currencies}
-                  onChange={handleCurrencySelection}
-                />
-                <InputGroup>
-                  {/* <InputLeftAddon>
-                    <Select
-                      defaultOptions
-                      value={selectedCurrency}
-                      options={currencies}
-                      onChange={handleCurrencySelection}
-                      maxWidth="2vw"
-                    />
-                  </InputLeftAddon> */}
-                  <Input
-                    placeholder={
-                      'amount in ' +
-                      selectedCurrency.symbolNative +
-                      ' (eg. 245)'
-                    }
-                    onChange={e => setEthAmount(e.target.value)}
+                <Skeleton isLoaded={!isLoading}>
+                  <Select
+                    defaultOptions
+                    value={selectedCurrency}
+                    options={currencies}
+                    onChange={handleCurrencySelection}
                   />
-                </InputGroup>
-
-                <Button
-                  colorScheme={'teal'}
-                  size={'lg'}
-                  onClick={() => donateEth()}
-                >
-                  Donate Funds
-                </Button>
+                </Skeleton>
+                <Skeleton isLoaded={!isLoading}>
+                  <InputGroup>
+                    <Input
+                      placeholder={
+                        'amount in ' +
+                        selectedCurrency.symbolNative +
+                        ' (eg. 245)'
+                      }
+                      onChange={e => setEthAmount(e.target.value)}
+                    />
+                  </InputGroup>
+                </Skeleton>
+                <Skeleton isLoaded={!isLoading}>
+                  <Button
+                    colorScheme={'teal'}
+                    size={'lg'}
+                    onClick={() => donateEth()}
+                  >
+                    Donate Funds
+                  </Button>
+                </Skeleton>
               </VStack>
+
               <VStack
                 spacing={'1vw'}
                 alignItems={'center'}
                 paddingRight={'2vw'}
               >
-                <Button
-                  colorScheme={'teal'}
-                  size={'lg'}
-                  onClick={e => viewCertDoc(e)}
-                >
-                  Tax Certificate
-                </Button>
-                <Button colorScheme={'teal'} size={'lg'}>
-                  Expense Report
-                </Button>
+                <Skeleton isLoaded={!isLoading}>
+                  <Button
+                    colorScheme={'teal'}
+                    size={'lg'}
+                    onClick={e => viewCertDoc(e)}
+                  >
+                    Tax Certificate
+                  </Button>
+                </Skeleton>
+                <Skeleton isLoaded={!isLoading}>
+                  <Button colorScheme={'teal'} size={'lg'}>
+                    Expense Report
+                  </Button>
+                </Skeleton>
               </VStack>
             </HStack>
           </Flex>
@@ -330,49 +362,64 @@ function CharityDetails() {
               width={'50vw'}
               paddingStart={'100px'}
             >
-              <Heading>Description</Heading>
+              <Skeleton mt="2vh" mb="2vh" isLoaded={!isLoading}>
+                <Heading>Description</Heading>
+              </Skeleton>
 
-              {charity.description ? (
-                <Text align={'justify'}>{charity.description}</Text>
-              ) : (
-                <Text align={'justify'}>
-                  Lorem ipsum dolor sit amet, consectetur adipisicing elit.
-                  Animi aperiam enim quo recusandae vero! Culpa hic impedit
-                  maxime quaerat voluptate. Debitis dolores fugiat harum
-                  laboriosam libero modi provident? Aliquam amet at autem
-                  corporis culpa cumque, dolore ea eius enim ex fugiat illo
-                  impedit in libero maiores minus nobis officia pariatur
-                  perferendis possimus qui quibusdam quisquam quos ratione rem
-                  repellat rerum sequi similique sint soluta tenetur totam vel
-                  voluptatem. Atque aut autem blanditiis consectetur cumque
-                  debitis delectus deleniti dolore doloribus et eveniet facere
-                  id ipsum maiores maxime molestias necessitatibus nisi, nostrum
-                  omnis quae qui quidem quisquam recusandae sit soluta sunt
-                  voluptas voluptatem! A ducimus eum eveniet id illum nisi saepe
-                  veritatis, voluptatum. Ab beatae culpa delectus dicta
-                  distinctio dolorem doloribus eaque earum eius enim est eveniet
-                  ex facere in, minima mollitia natus obcaecati odio odit
-                  officia optio perspiciatis quasi quidem sunt voluptates? A ad
-                  aliquam autem cupiditate debitis doloremque dolores, fugit
-                  ipsa labore optio perspiciatis reiciendis sunt voluptates?
-                  Aliquam eligendi eos error hic in non repudiandae saepe
-                  temporibus voluptatum. Ab animi autem eos nihil perspiciatis
-                  quae quibusdam repellendus rerum? Culpa, cum delectus dolorem
-                  doloribus eligendi facere fugit incidunt laborum odio
-                  perspiciatis placeat porro quas quasi quibusdam repudiandae
-                  unde, voluptates? Accusamus, distinctio?
-                </Text>
-              )}
+              <SkeletonText isLoaded={!isLoading} noOfLines={12} spacing="6">
+                {charity.description ? (
+                  <Text align={'justify'}>{charity.description}</Text>
+                ) : (
+                  <Text align={'justify'}>
+                    Lorem ipsum dolor sit amet, consectetur adipisicing elit.
+                    Animi aperiam enim quo recusandae vero! Culpa hic impedit
+                    maxime quaerat voluptate. Debitis dolores fugiat harum
+                    laboriosam libero modi provident? Aliquam amet at autem
+                    corporis culpa cumque, dolore ea eius enim ex fugiat illo
+                    impedit in libero maiores minus nobis officia pariatur
+                    perferendis possimus qui quibusdam quisquam quos ratione rem
+                    repellat rerum sequi similique sint soluta tenetur totam vel
+                    voluptatem. Atque aut autem blanditiis consectetur cumque
+                    debitis delectus deleniti dolore doloribus et eveniet facere
+                    id ipsum maiores maxime molestias necessitatibus nisi,
+                    nostrum omnis quae qui quidem quisquam recusandae sit soluta
+                    sunt voluptas voluptatem! A ducimus eum eveniet id illum
+                    nisi saepe veritatis, voluptatum. Ab beatae culpa delectus
+                    dicta distinctio dolorem doloribus eaque earum eius enim est
+                    eveniet ex facere in, minima mollitia natus obcaecati odio
+                    odit officia optio perspiciatis quasi quidem sunt
+                    voluptates? A ad aliquam autem cupiditate debitis doloremque
+                    dolores, fugit ipsa labore optio perspiciatis reiciendis
+                    sunt voluptates? Aliquam eligendi eos error hic in non
+                    repudiandae saepe temporibus voluptatum. Ab animi autem eos
+                    nihil perspiciatis quae quibusdam repellendus rerum? Culpa,
+                    cum delectus dolorem doloribus eligendi facere fugit
+                    incidunt laborum odio perspiciatis placeat porro quas quasi
+                    quibusdam repudiandae unde, voluptates? Accusamus,
+                    distinctio?
+                  </Text>
+                )}
+              </SkeletonText>
             </VStack>
             <VStack spacing={8} px={12} marginTop={90}>
-              <Heading alignSelf={'start'} paddingTop={'5'}>
-                Latest Expenses
-              </Heading>
+              <Skeleton
+                alignSelf={'start'}
+                paddingTop={'5'}
+                w={'20vw'}
+                mt="2vh"
+                h={'7vh'}
+                isLoaded={!isLoading}
+              >
+                <Heading alignSelf={'start'} paddingTop={'5'}>
+                  Latest Expenses
+                </Heading>
+              </Skeleton>
               {expenses.map(expense => (
                 <ReportCard
                   title={expense.title}
                   date={expense.date}
                   value={expense.value}
+                  isLoaded={!isLoading}
                 />
               ))}
             </VStack>
--- src/Components/ReportCard.js
+++ src/Components/ReportCard.js
@@ -4,32 +4,42 @@ import {
   Text,
   useColorModeValue,
   VStack,
+  Skeleton,
+  SkeletonText,
 } from '@chakra-ui/react';
 
 export default function ReportCard(props) {
+  const isLoaded = props.isLoaded;
   return (
     <>
       <Stack
-        borderWidth='1px'
-        borderRadius='lg'
+        borderWidth="1px"
+        borderRadius="lg"
         w={{ sm: '100%', md: '50rem' }}
         height={{ sm: '476px', md: '8rem' }}
         direction={{ base: 'column', md: 'row' }}
         bg={useColorModeValue('white', 'gray.800')}
         boxShadow={'2xl'}
-        padding={4}>
+        padding={4}
+      >
         <HStack width={'40vw'} justifyContent={'space-between'}>
           <VStack spacing={2} align={'start'} padding={10}>
-            <Text as='b' fontSize='xl'>
-              { props.title }
-            </Text>
-            <Text as='i' fontSize='sm'>
-              { props.date }
-            </Text>
+            <Skeleton isLoaded={isLoaded}>
+              <Text as="b" fontSize="xl">
+                {props.title}
+              </Text>
+            </Skeleton>
+            <SkeletonText noOfLines={1} isLoaded={isLoaded}>
+              <Text as="i" fontSize="sm">
+                {props.date}
+              </Text>
+            </SkeletonText>
           </VStack>
-          <Text as='b' fontSize='4xl' align='left'  padding={10}>
-            $ { props.value }
-          </Text>
+          <Skeleton isLoaded={isLoaded} m="2">
+            <Text as="b" fontSize="4xl" align="left" padding={10}>
+              $ {props.value}
+            </Text>
+          </Skeleton>
         </HStack>
       </Stack>
     </>
