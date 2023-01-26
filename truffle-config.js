900d4b01f955ec6c8a6432f540b4cc979539a155
jaypowar00 <jaypowar00@gmail.com>
2023-01-26 18:55:08 +0530

    [+] added smart contract provider for infura in truffle-config.js

--- truffle-config.js
+++ truffle-config.js
@@ -1,11 +1,13 @@
+require('dotenv').config()
+const HDWalletProvider = require('@truffle/hdwallet-provider');
+const { INFURA_API_KEY, MNEMONIC } = process.env;
 module.exports = {
   networks: {
-    development: {
-    //  host: "127.0.0.1",
-     host: "avdhut.live",
-     port: 8545,
-     network_id: "*",
-    },
+    goerli: {
+      provider: () => new HDWalletProvider(MNEMONIC, INFURA_API_KEY),
+      network_id: '5',
+      gas: 4465030
+    }
   },
 
   contracts_directory: './src/contracts/',
